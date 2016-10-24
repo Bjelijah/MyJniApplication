b76dc60764c366e3f46077d60058b707a25a6a7b
Bjelijah <elijah@live.cn>
2016-10-24 16:56:20 +0800

    add .so but in target api 22

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,14 +1,22 @@
 LOCAL_PATH := $(call my-dir)
 
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := myadd
+#LOCAL_SRC_FILES := libmyadd.a
+#include $(PREBUILT_STATIC_LIBRARY)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := myadd
-LOCAL_SRC_FILES := libmyadd.a
-include $(PREBUILT_STATIC_LIBRARY)
+LOCAL_SRC_FILES := libmyadd.so
+LOCAL_CFLAGS := -fPIC
+include $(PREBUILT_SHARED_LIBRARY)
+
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := testjni
 LOCAL_SRC_FILES := testJni.cpp
-LOCAL_STATIC_LIBRARIES := myadd
+#LOCAL_STATIC_LIBRARIES := myadd
+LOCAL_SHARED_LIBRARIES := myadd
 LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
